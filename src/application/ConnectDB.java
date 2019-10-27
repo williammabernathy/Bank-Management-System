8e381bd0ffaee72f5fc48513f11c3d0af7ac13f2
Igifoshifo <matthew.abernathy@hotmail.com>
2019-10-27 13:44:07 -0400

    Initial commit. Database Connection set up

---
+++ src/application/ConnectDB.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.sql.Connection ;
+import java.sql.DriverManager ;
+
+public class ConnectDB
+{
+    private static Connection connection;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String dbConnection = "jdbc:mysql://127.0.0.1:3306/bankmanagementsystem";
+    private static final String username = "root";
+    private static final String password = "root";
+
+    public ConnectDB()
+    {
+
+    }
+
+    public static Connection setupConnection()
+    {
+
+        try
+        {
+            Class.forName(driver);
+        }
+        catch (Exception e)
+        {
+            //System.out.println("catch driver");
+        }
+
+        try
+        {
+            connection = DriverManager.getConnection(dbConnection, username, password);
+            return connection;
+        }
+        catch (Exception e)
+        {
+            //System.out.println("catch connection");
+        }
+
+        return connection;
+    }
+}
---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+
+public class Main extends Application
+{
+    private static Connection connection;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        connection = ConnectDB.setupConnection();
+
+        //System.out.println(connection);
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
